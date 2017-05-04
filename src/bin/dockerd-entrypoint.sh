81f35fc2d0c0c2c17770b52681134b1eaf7b097b
Sascha Marcel Schmidt <torvitas@users.noreply.github.com>
2017-05-04 02:19:42 +0200

    Revert "Clean up processes so they don't hang GitLab CI builds"

--- src/bin/dockerd-entrypoint.sh
+++ src/bin/dockerd-entrypoint.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo Command required.
-  exit 1
-fi
-
 supervisord -c /usr/local/etc/supervisor.conf &
-SUPERVISOR_PID=$!
-
-PROCESS_PID=
-
-on_exit() {
-  exit_code=$1
-  if [ -n "${PROCESS_PID}" ]; then
-    kill -TERM ${PROCESS_PID}
-    wait ${PROCESS_PID}
-    PROCESS_PID=
-  fi
-  if [ -n "${SUPERVISOR_PID}" ]; then
-    kill -TERM ${SUPERVISOR_PID}
-    wait ${SUPERVISOR_PID}
-    SUPERVISOR_PID=
-  fi
-  exit $exit_code
-}
-trap 'on_exit 0' EXIT
-trap 'on_exit 1' TERM INT
 
 is_docker_up() {
   curl --unix-socket /var/run/docker.sock -o /dev/null --silent --fail http://docker/_ping
@@ -44,9 +19,4 @@ if ! is_docker_up; then
   exit 1
 fi
 
-"$@" &
-PROCESS_PID=$!
-wait $PROCESS_PID
-exit_code=$?
-PROCESS_PID=
-on_exit $exit_code
+exec "$@"
